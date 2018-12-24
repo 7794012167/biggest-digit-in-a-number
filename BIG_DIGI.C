5b4e8d52c0f765cb470888efb9432e10cb511741
7794012167 <46022823+7794012167@users.noreply.github.com>
2018-12-24 17:53:00 +0530

    Add files via upload

---
+++ BIG_DIGI.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,r,big=11;
+	clrscr();
+	printf("enter a value");
+	scanf("%d",&a);
+	while(a!=0)
+	{
+		r=a%10;
+		a=a/10;
+		if(big>dr)
+		{
+		  big=r;
+		}
+	}
+		printf("%d",big);
+
+	 getch();
+}
